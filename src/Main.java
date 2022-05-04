5dff5036efe5c351d33701737580d36f730947df
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-05-04 20:19:50 +0100

    adding

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore(true, 800, levelCompleted, bonus);
+
+
+        score = 1000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("final score was " + finalScore);
+        }
+
     }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+            System.out.println("");
+        }
+
+    }
+
+
 }
+
